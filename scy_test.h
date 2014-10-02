7a6858f4723b85522b62a592950eddfeb3509e89
songcy <songcy6202@gmail.com>
2014-10-02 22:02:12 +0800

    update scy_test.h binarytree

--- scy_test.h
+++ scy_test.h
@@ -2,7 +2,7 @@
  * filename	:  scy_test.h
  * author	:  songcy
  * email	:  920828276@qq.com
- * last modified :  2014-09-11 09:36
+ * last modified :  2014-10-01 11:09
  * description	:  
 ************************************************/
 #ifndef _SCY_TEST_H_
@@ -18,6 +18,9 @@
 
 namespace scy_test
 {
+
+	
+
 	/***************************
 	*	judge wether the array is sorted
 	***************************/
@@ -28,7 +31,7 @@ namespace scy_test
 		{
 			if(*i < *(i-1))
 			{
-#ifndef TEST
+#ifndef NOTEST
 				std::cout<< *i <<" < " <<*(i-1)<<std::endl;
 				std::cout<<" unsorted ! "<<std::endl;
 #endif
@@ -98,12 +101,12 @@ namespace scy_test
 		~timer()
 		{
 			gettimeofday(&tv,NULL);
-			endtime = tv.tv_sec * 1000 + tv.tv_usec/1000;
+			endtime = tv.tv_sec*1000 + tv.tv_usec/1000;
 			std::cout<<"- running time : "<<endtime-starttime<<" ms "<<std::endl;
 			std::cout<<"--------------------------"<<std::endl;
 		}
 	private:
-		struct timeval tv;  //to create time in 0.001
+		struct timeval tv;  //to create time in 0.001s
 		time_t starttime;
 		time_t endtime;
 		std::string info;
@@ -185,6 +188,118 @@ namespace scy_test
 		const int max_value;
 		static const int defaultsize = 10000;
 //		static unsigned randseed;
+	};
+
+	/**********************************************
+	 * TODO:create binary tree
+	 ***********************************************/
+	template<typename T>
+	class Node
+	{
+		public:
+			Node *left;
+			Node *right;
+			T val;
+	};
+	template<typename T>
+	class BinaryTree
+	{
+		public:
+			void traverseTree(void (*fun)());
+			size_t size()
+			{
+				return node_count;
+			}
+			BinaryTree(size_t nodenum, int max):
+				node_count(nodenum),
+				maxval(max),
+				nodevec(0)
+			{
+				createRandomBinaryTree();
+			}
+			~BinaryTree()
+			{
+				for(int i = 0; i < nodevec.size(); ++i)
+					delete nodevec[i];
+			}
+			Node<T>* getRoot()
+			{
+				return root;
+			}
+			void printBinaryTree()
+			{
+				int count = 0;
+				traverseTree(root, count);
+			}
+		private:
+			void createRandomBinaryTree()
+			{
+				int count = 0;
+				RandomArray<int> randarray(node_count, maxval);	
+				std::vector<int> ivec =  randarray.getUnsortedArray();
+				root = new Node<T>;
+				root->left = root->right = NULL;
+				root->val = ivec[0];
+				createRandomBinaryTreeChild(root, ivec, 1);
+			}
+
+			void createRandomBinaryTreeChild(Node<T> *node, std::vector<int>& ivec, int index)
+			{
+				std::cout<<"index: "<<index<<std::endl;
+				if(index >= ivec.size()-1)
+					return;
+				int ranleft = rand()%2;
+				int ranright = rand()%2;
+				if(ranleft == 0)
+				{
+					Node<T> *newnode = new Node<T>;
+					nodevec.push_back(newnode);
+					newnode->val = ivec[index];
+					newnode->left = NULL;
+					newnode->right = NULL;
+					node->left = newnode;
+					createRandomBinaryTreeChild(node->left, ivec, index+1);
+					index++;
+				}
+				if(ranright == 0)
+				{
+					Node<T> *newnode = new Node<T>;
+					nodevec.push_back(newnode);
+					newnode->val = ivec[index];
+					newnode->left = NULL;
+					newnode->right = NULL;
+					node->right = newnode;
+					createRandomBinaryTreeChild(node->right, ivec, index+1);
+				}
+			}
+			void traverseTree(Node<T>* node,int count)
+			{
+				if(node == NULL)
+					return;
+
+				if(node->left)
+					std::cout<<node->left->val<<"<-";
+				else
+					std::cout<<"NULL<-";
+				std::cout<<node->val;
+				if(node->right)
+					std::cout<<"->"<<node->right->val;
+				else
+					std::cout<<"->NULL";
+				std::cout<<"\t";
+				printf("deep =  %d\n",count);
+
+				count++;
+				traverseTree(node->left,count);
+				traverseTree(node->right,count);
+
+			}
+//			void createSortedBinaryTree();  //TODO
+//			void createFullBinaryTree();    //TODO
+			std::vector< Node<T>* > nodevec;
+			size_t node_count;
+			int maxval;
+			Node<T> *root;
 	};
 
 }
